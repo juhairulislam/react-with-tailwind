1aec0b0268657db57aeac7f827a5990506cdb817
juhairulislam <juhairulislam2018@gmail.com>
2026-03-18 09:57:59 +0600

    trying to push

--- src/Components/PricingOptions/PricingOptions.jsx
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -8,7 +8,7 @@ const PricingOptions = ({pricingPromise}) => {
     return (
       <div>
         <h2 className='text-5xl text-center mt-10'>Get Our MemberShip </h2> 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 p-5'>
             {
                 pricingData.pricingPlans.map(pricing=><PricingCard pricing={pricing}
                 key={pricing.id}></PricingCard>)
